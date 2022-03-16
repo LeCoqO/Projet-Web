d0269d667edc96b184e4567888eef177e1ae8234
Quentin L <quentin.lustiere.auditeur@lecnam.net>
2022-03-16 11:40:21 +0100

    Create AccueilGerant.php

---
+++ AccueilGerant.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta name="author" content="Diego TORRES" />
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>BulgarKing</title>
+</head>
+<header>
+    <div class="sidebar" id="mySidebar">
+        <button class="bar-item button" onclick="sidebar_close()">Close &times;</button>
+        <a href="commande.php" class="bar-item button">Commande</a><br>
+        <a href="recette.php" class="bar-item button">Link 2</a><br>
+        <a href="#" class="bar-item button">Link 3</a>
+    </div>
+    <button class="button left hide-large" onclick="sidebar_open()">&#9776;</button>
+    <h1 class="text-center ">
+        <img src="logo.png" class="logo" alt="" />
+    </h1>
+
+</header>
+
+<body>
+    <hr>
+    <div>
+        <br>
+        <button class="boutonMenuGerant">Statistiques</button>
+
+        <button class="boutonMenuGerant">Stocks</button>
+
+        <button class="boutonMenuGerant">Recettes</button>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
