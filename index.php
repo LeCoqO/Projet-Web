7dc2b8256dc02dee44e36e8e361bd913d9f2f134
Quentin L <quentin.lustiere.auditeur@lecnam.net>
2022-03-16 10:46:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta name="author" content="Diego TORRES" />
+    <link rel="stylesheet" href="Diegz/style.css">
+    <script     src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>BulgarKing</title>
+</head>
+<header>
+    <div class="sidebar" id="mySidebar">
+        <button class="bar-item button" onclick="sidebar_close()">Close &times;</button>
+        <a href="commande.php" class="bar-item button">Commande</a><br>
+        <a href="recette.php" class="bar-item button">Link 2</a><br>
+        <a href="#" class="bar-item button">Link 3</a>
+    </div>
+    <button class="button left hide-large" onclick="sidebar_open()">&#9776;</button>
+    <h1 class="text-center ">
+        <img src="diegz/img/logo.jpg" class="logo" alt="" />
+    </h1>
+
+</header>
+
+<body>
+
+            <button>Suivi Livraisons</button>
+
+            <button>Emmetre bon de commande</button>
+    
+            <button>Voir bons de commandes</button>
+    
+            <hr>
+    
+    
+
+
+    
+</body>
+
+</html>
