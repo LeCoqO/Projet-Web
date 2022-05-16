a1fe3f5fb22cdc7112947016607f7e719633fff6
Kyuu71 <quentin.lustiere.auditeur@lecnam.net>
2022-05-16 13:59:54 +0200

    bonCommande IHM
    reste partie PHP PDF

--- Q/Bons_De_Commandes.php
+++ Q/Bons_De_Commandes.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta name="author" content="Diego TORRES" />
     <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>BulgarKing</title>
 </head>
 <header>
@@ -49,7 +49,7 @@
             <div>
                 <article>
                     <div class="left">
-                        <h3><a href="">link text</a></h3>
+                        <h3><a href="" id="PDF">link text</a></h3>
                         <div class="bonCompact">
                             <div class="left">Date d'émission : XX/XX/XXXX</div>
                             <div class="right">Date livraison prévue : XX/XX/XXXX</div>
@@ -64,6 +64,115 @@
                     <div class="right vertical-center">
                         <input class="" type="checkbox" checked>
                     </div>
+                </article>
+                <br>
+                <br>
+                <hr>
+                <div id="parent" class="clear">
+                </div>
+
+
+                <script>
+                    var laFonction = $.ajax({
+                        url: 'STOCK_REQUETE.php', //toujours la même page qui est appelée
+                        type: 'POST',
+                        data: {
+                            fonction: 'select', //fonction à executer
+                            requete: 'SELECT * FROM commandefournisseur', //On a besoin de TOUS les champs de commandefournisseur, d'ou l'utilisation du * 
+                        }
+                    });
+
+                    laFonction.done(function(msg) {
+                        let resultats = JSON.parse(msg);
+                        let resultats2;
+                        let autreReq = [];
+
+                        for (var IdIng in resultats) {
+                            autreReq.push(IdIng);
+                        }
+
+                        var laFonction2 = $.ajax({
+                            url: 'STOCK_REQUETE.php', //toujours la même page qui est appelée
+                            type: 'POST',
+                            data: {
+                                fonction: 'select', //fonction à executer
+                                requete: 'SELECT NomIng,IdIng,Unite,PrixUHT_Moyen AS PUHT FROM ingredient WHERE IdIng IN (SELECT IdIng FROM commandefournisseur)',
+                            }
+                        });
+                        laFonction2.done(function(msg2) {
+                            resultats2 = JSON.parse(msg2);
+                            for (i = 0; i < resultats.length; i++) {
+
+                                let laCommande = document.createElement('article');
+                                let h3 = document.createElement('h3');
+                                let button = document.createElement('button');
+                                let tousLesChamps = document.createElement('div');
+                                let dateE = document.createElement('div');
+                                let dateL = document.createElement('div');
+                                let ing = document.createElement('div');
+                                let qte = document.createElement('div');
+                                let fourn = document.createElement('div');
+                                let prix = document.createElement('div');
+
+                                button.innerHTML = "link text";
+                                dateE.innerHTML = "Date d'émission : " + resultats[i]['DateComFourn'];
+                                dateL.innerHTML = "Date livraison prévue : " + resultats[i]['DateLivFourn'];
+                                ing.innerHTML = resultats2[i]['NomIng'];
+                                qte.innerHTML = resultats[i]['QteComFourn'] + ' ' + resultats2[i]['Unite'];
+                                fourn.innerHTML = resultats[i]['NomFourn'];
+                                prix.innerHTML = 'PRIX TOTAL HT : ' + resultats[i]['QteComFourn'] * resultats2[i]['PUHT'] + ' €';
+
+                                laCommande.className = 'left';
+                                button.id = 'PDF' + resultats[i]['IdComFourn'];
+                                button.href = "https://www.google.com/";
+                                button.className = 'button toPDF'
+                                tousLesChamps.className = 'bonCompact';
+                                dateE.className = 'left';
+                                dateL.className = 'right';
+                                ing.className = 'clear left';
+                                qte.className = 'right';
+                                fourn.className = 'clear';
+                                prix.className = 'text-center';
+
+                                laCommande.appendChild(h3);
+                                h3.appendChild(button);
+                                tousLesChamps.appendChild(dateE);
+                                tousLesChamps.appendChild(dateL);
+                                tousLesChamps.appendChild(ing);
+                                tousLesChamps.appendChild(qte);
+                                tousLesChamps.appendChild(fourn);
+                                tousLesChamps.appendChild(prix);
+                                laCommande.appendChild(tousLesChamps);
+
+                                button.addEventListener('click', event => {
+                                    console.log(button.id); ///PDF
+                                });
+
+                                parent = document.getElementById('parent');
+                                parent.appendChild(laCommande);
+                            }
+                        });
+                        laFonction.fail(function(dataSQL, statut) {
+                            alert("error sqlConnect.js : " + dataSQL.erreur);
+                        });
+                        /* 
+                        let resultats = JSON.parse(msg);
+                        selectIng = document.createElement('select');
+                        selectIng[0] = new Option("--Ingrédient--", "", false, false);
+                        for (i = 0; i < resultats.length; i++) {
+                            selectIng[i + 1] = new Option(resultats[i]['NomIng'], resultats[i]['IdIng'], false, false);
+                        };
+                        selectIng.id = 'selectIng';
+                        selectIng.class = 'column';
+                        selectIng.onChange = 'appel(this.value)';
+                        document.getElementById('requete').appendChild(selectIng);
+                        */
+
+                    });
+                    laFonction.fail(function(dataSQL, statut) {
+                        alert("error sqlConnect.js : " + dataSQL.erreur);
+                    });
+                </script>
                 </article>
             </div>
         </main>
